0a17861cca103724054e2a800837dfcdf0a29952
Martin Høgh <mh@mapcentia.com>
2021-03-15 11:21:46 +0100

    configs

---
+++ src/config.prod.js
@@ -0,0 +1,6 @@
+const config = {
+    apiUrl: `https://geofa-test.admin.gc2.io/`,
+    homepage: `/editor/`
+};
+
+export default config;
--- src/config.test.js
+++ src/config.test.js
@@ -1,5 +1,4 @@
 const config = {
-    //apiUrl: `https://geofa-test.admin.gc2.io/`
     apiUrl: `http://localhost:8080/`,
     homepage: `/editor/`
 };
