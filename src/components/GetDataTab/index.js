c4f50d6347d775e5a5f3e64d9e52d38d506e1e83
MapCentia <mh@mapcentia.com>
2021-03-18 12:39:30 +0100

    Update index.js

--- src/components/GetDataTab/index.js
+++ src/components/GetDataTab/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import Button from '@material-ui/core/Button';
+import LaunchIcon from '@material-ui/icons/Launch';
+import StyledExternalLink from './../../components/StyledExternalLink';
 import {createStructuredSelector} from "reselect";
 import {connect} from "react-redux";
+import {makeSelectUser, makeSelectGC2Configuration} from "../../containers/App/selectors";
+import config from "../../config";
 
 class GetDataTab extends React.Component {
     constructor(props) {
@@ -21,18 +26,29 @@ class GetDataTab extends React.Component {
                             GeoFA databasen.</p>
                     </Typography>
                     <Typography variant="h6" color="inherit">
-                        Se og editer data
+                        Se data
                     </Typography>
                     <Typography variant="body1" color="inherit" style={{paddingTop: `10px`}}>
-                        <p>Her kan du se og editere enkelt-objekter i webkort-løsning.</p>
+                        <p>Her kan du se og (og senere editere) enkelt-objekter i webkort-løsning.</p>
+                        <StyledExternalLink href={`${this.props.gc2Configuration.gc2Options.vidiUrl}/app/fkg/fkg`} target="_blank" style={{marginRight: `10px`}}>
+                            <Button color="primary" variant="contained" size="small">
+                                <LaunchIcon/> Start kortet
+                            </Button>
+                        </StyledExternalLink>
                         <p>Vejledning</p>
                     </Typography>
                     <Typography variant="h6" color="inherit">
                         WFS og WFS-T
                     </Typography>
                     <Typography variant="body1" color="inherit" style={{paddingTop: `10px`}}>
-                        <p>WFS 2.0: ....</p>
-                        <p>WFS-t 1.1: ....</p>
+                        <p>WFS 2.0: {config.apiUrl}ows/fkg/fkg</p>
+                        <p>WFS-t 1.1: {config.apiUrl}wfs/{this.props.user.screenName}@fkg/fkg/25832</p>
+                    </Typography>
+                    <Typography variant="h6" color="inherit">
+                        Download af datafiler
+                    </Typography>
+                    <Typography variant="body1" color="inherit" style={{paddingTop: `10px`}}>
+                        <p>Det bliver muligt at download data i Geopackage formatet.</p>
                     </Typography>
 
                 </Grid>
@@ -41,6 +57,9 @@ class GetDataTab extends React.Component {
     }
 }
 
-const mapStateToProps = createStructuredSelector({});
+const mapStateToProps = createStructuredSelector({
+    user: makeSelectUser(),
+    gc2Configuration: makeSelectGC2Configuration(),
+});
 
 export default connect(mapStateToProps)(GetDataTab);
