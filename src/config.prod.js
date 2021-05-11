00d35148bd91a1108aa0264d98e7edcffd40ea17
Martin Høgh <mh@mapcentia.com>
2021-05-11 08:23:24 +0200

    Prod and test photo urls

--- src/config.prod.js
+++ src/config.prod.js
@@ -2,7 +2,8 @@ const config = {
     prod: true,
     apiUrl: `https://geofa.geodanmark.dk/`,
     homepage: `/editor/`,
-    vidiUri: `/app/fkg/?config=/api/v2/configuration/fkg/configuration_fkg_udgivet_5f465f5d3181f687353260.json`
+    vidiUri: `/app/fkg/?config=/api/v2/configuration/fkg/configuration_fkg_udgivet_5f465f5d3181f687353260.json`,
+    vidiUriPhoto: `/app/fkg/?config=/api/v2/configuration/fkg/configuration_tilknyt_billeder_608bd5048bfef734424106.json`,
 };
 
 export default config;
--- src/config.test.js
+++ src/config.test.js
@@ -2,7 +2,8 @@ const config = {
     prod: false,
     apiUrl: `https://geofa-test.geodanmark.dk/`,
     homepage: `/editor/`,
-    vidiUri: `/app/fkg/?config=/api/v2/configuration/fkg/configuration_fkg_udgivet_5f465f5d3181f687353260.json`
+    vidiUri: `/app/fkg/?config=/api/v2/configuration/fkg/configuration_fkg_udgivet_5f465f5d3181f687353260.json`,
+    vidiUriPhoto: `/app/fkg/?config=/api/v2/configuration/fkg/configuration_tilknyt_billeder_609933b8790e9035367682.json`,
 };
 
 export default config;
