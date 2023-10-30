9bca95ab92ebbf7b3be2517307e49bc18dc43d4f
Martin Høgh <mh@mapcentia.com>
2023-10-30 12:43:02 +0100

    Add some GUI param to photo Vidi

--- src/config.test.js
+++ src/config.test.js
@@ -3,7 +3,7 @@ const config = {
     apiUrl: `https://geofa-test.geodanmark.dk/`,
     homepage: `/editor/`,
     vidiUri: `/app/fkg/?config=/api/v2/configuration/fkg/configuration_fkg_udgivet_5f465f5d3181f687353260.json`,
-    vidiUriPhoto: `/app/fkg/?config=/api/v2/configuration/fkg/configuration_tilknyt_billeder_609933b8790e9035367682.json`,
+    vidiUriPhoto: `/app/fkg/?config=/api/v2/configuration/fkg/configuration_tilknyt_billeder_609933b8790e9035367682.json&leg=none&abo=none&mea=none&cle=none&scr=none`,
 };
 
 export default config;
