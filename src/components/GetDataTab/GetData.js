8178a77fb9b6698451bd578a43d7a5a40d282e6d
Martin Høgh <mh@mapcentia.com>
2024-09-02 14:14:19 +0200

    5606 added

--- src/components/GetDataTab/GetData.js
+++ src/components/GetDataTab/GetData.js
@@ -61,6 +61,7 @@ class GetData extends React.Component {
                     <option value={'t_5603_hasti_omraade'}>t_5603_hasti_omraade</option>
                     <option value={'t_5604_koer_begr'}>t_5604_koer_begr</option>
                     <option value={'t_5605_vejbyggel'}>t_5605_vejbyggel</option>
+                    <option value={'t_5606_vejinv'}>t_5606_vejinv</option>
                     <option value={'t_5607_ladefacilitet'}>t_5607_ladefacilitet</option>
                     <option value={'t_5608_cykelknudepunkter'}>t_5608_cykelknudepunkter</option>
                     <option value={'t_5609_cykelknudepunktsstraekninger'}>t_5609_cykelknudepunktsstraekninger</option>
