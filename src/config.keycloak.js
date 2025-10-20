f39400fbb243b710ca89c65bf2d291296ac21706
Martin Høgh <mh@mapcentia.com>
2025-10-20 12:40:11 +0200

    Update

---
+++ src/config.keycloak.js
@@ -0,0 +1,9 @@
+const config = {
+    prod: false,
+    apiUrl: `https://geofa-keycloak.admin.gc2.io/`,
+    homepage: `/editor/`,
+    vidiUri: `/app/fkg/?config=/api/v2/configuration/fkg/configuration_fkg_udgivet_5f465f5d3181f687353260.json`,
+    vidiUriPhoto: `/app/fkg/?config=/api/v2/configuration/fkg/configuration_tilknyt_billeder_609933b8790e9035367682.json&leg=none&abo=none&mea=none&cle=none&scr=none`,
+};
+
+export default config;
